470d85479694969b17dc286c68aa10b47d24be55
Giacinto <g.tucciarone@solvingteam.it>
2021-03-16 13:38:33 +0100

    refactor

--- src/test/java/io/data/encryption/EncryptionApplicationTests.java
+++ src/test/java/io/data/encryption/EncryptionApplicationTests.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.util.Assert;
 
 import javax.crypto.IllegalBlockSizeException;
 
